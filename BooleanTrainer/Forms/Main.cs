93b5119223da8bd68e598cf50c0e7100407f38a4
frexxx-7 <borzenkokiril9@gmail.com>
2023-10-17 11:29:28 +0300

    Добавьте файлы проекта.

---
+++ BooleanTrainer/Forms/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooleanTrainer.Forms
+{
+    public partial class Main : Form
+    {
+        public static string login;
+        private string id;
+        public Main(string id)
+        {
+            InitializeComponent();
+            this.id = id;
+        }
+    }
+}
